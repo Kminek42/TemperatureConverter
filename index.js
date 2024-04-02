9321189fffd4dc6ffaaf4d354c40755f7a625135
Kminek <k.kminkowski@icloud.com>
2024-04-02 16:00:53 +0200

    done

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function C2F(value) {
-    return value * 9 / 5 + 32;
+    temp = value * 9 / 5 + 32;
+    temp = Math.round(100 * temp) / 100;
+    return temp;
 }
 
 function F2C(value) {
-    return (value - 32) * 5 / 9;
+    temp =  (value - 32) * 5 / 9;
+    temp = Math.round(100 * temp) / 100;
+    return temp;
 }
 
 function conversion() {
     if (!isNaN(inp.value) && inp.value != '') {
-        var temp = C2F(inp.value);
-        temp = Math.round(100 * temp) / 100;
+        if (CtoF) {
+            var temp = C2F(inp.value);
+        }
+        else {
+            var temp = F2C(inp.value);
+        }
         out.setAttribute('value', temp);
     }
 
@@ -18,8 +26,27 @@ function conversion() {
     }
 }
 
+function switchHandler() {
+    if (CtoF) {
+        CtoF = false;
+        inputText.innerHTML = 'Temperature °F';
+        outputText.innerHTML = 'Temperature °C';
+    }
+    else {
+        CtoF = true;
+        inputText.innerHTML = 'Temperature °C';
+        outputText.innerHTML = 'Temperature °F';
+    }
+    conversion();
+}
 
 var inp = document.getElementById('input');
 var out = document.getElementById('output');
+var inputText = document.getElementById('inputText');
+var outputText = document.getElementById('outputText');
+
+var CtoF = true;
 
 document.getElementById('input').addEventListener('input', conversion);
+document.getElementById('switch').addEventListener('click', switchHandler);
+document.onload(conversion());
