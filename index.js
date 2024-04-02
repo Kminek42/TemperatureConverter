cdaf5128683383fdbf77f71964150a8c047be5e5
Kminek <k.kminkowski@icloud.com>
2024-04-02 15:36:05 +0200

    better look

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function C2F() {
-    var inp = document.getElementById('input').value;
-    var out = document.getElementById('output');
+function C2F(value) {
+    return value * 9 / 5 + 32;
+}
 
-    if (!isNaN(inp) && inp != '') {
-        var temp = (inp - 32) * 5 / 9;
-        temp = Math.round(10 * temp) / 10;
+function F2C(value) {
+    return (value - 32) * 5 / 9;
+}
+
+function conversion() {
+    if (!isNaN(inp.value) && inp.value != '') {
+        var temp = C2F(inp.value);
+        temp = Math.round(100 * temp) / 100;
         out.setAttribute('value', temp);
     }
 
@@ -13,8 +18,8 @@ function C2F() {
     }
 }
 
-function greet() {
-    alert('Hello, World!');
-}
 
-document.getElementById('input').addEventListener('input', C2F);
+var inp = document.getElementById('input');
+var out = document.getElementById('output');
+
+document.getElementById('input').addEventListener('input', conversion);
